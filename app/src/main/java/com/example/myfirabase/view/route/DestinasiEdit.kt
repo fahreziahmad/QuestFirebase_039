118072862ec61dc8c97d728f0eab34c4306b00b9
fahreziahmad <alfahreziahmadsyahyan@gmail.com>
2026-01-02 18:17:27 +0700

    menambahkan destinasi edit siswa dengan parameter id

---
+++ app/src/main/java/com/example/myfirabase/view/route/DestinasiEdit.kt
@@ -0,0 +1,10 @@
+package com.example.myfirabase.view.route
+
+import com.example.contohfirebase.R
+
+object DestinasiEdit : DestinasiNavigasi {
+    override val route = "item_edit"
+    override val titleRes = R.string.edit_siswa
+    const val itemIdArg = "idSiswa"
+    val routeWithArgs = "$route/{$itemIdArg}"
+}
