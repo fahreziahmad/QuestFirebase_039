e1aebfa8fede31d4f2945b4c9379966218fa963e
fahreziahmad <alfahreziahmadsyahyan@gmail.com>
2026-01-02 18:39:59 +0700

    Menambahkan DefaultContainerApp sebagai penyedia repository siswa

--- app/src/main/java/com/example/myfirabase/repositori/ContainerApp.kt
+++ app/src/main/java/com/example/myfirabase/repositori/ContainerApp.kt
@@ -6,3 +6,9 @@ import android.app.Application
 interface ContainerApp {
     val repositorySiswa: RepositorySiswa
 }
+
+class DefaultContainerApp : ContainerApp {
+    override val repositorySiswa: RepositorySiswa by lazy {
+        FirebaseRepositorySiswa()
+    }
+}
