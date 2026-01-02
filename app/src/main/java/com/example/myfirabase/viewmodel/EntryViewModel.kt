a08c125f17bec51f4faeff621a825b814c16f2e3
fahreziahmad <alfahreziahmadsyahyan@gmail.com>
2026-01-02 18:21:05 +0700

    menambahkan fungsi validasi input siswa

--- app/src/main/java/com/example/myfirabase/viewmodel/EntryViewModel.kt
+++ app/src/main/java/com/example/myfirabase/viewmodel/EntryViewModel.kt
@@ -11,3 +11,10 @@ import com.example.myfirabase.modeldata.toDataSiswa
 class EntryViewModel(private val repositorySiswa: RepositorySiswa) : ViewModel() {
     var uiStateSiswa by mutableStateOf(UIStateSiswa())
         private set
+
+    /* Fungsi untuk memvalidasi input */
+    private fun validasiInput(uiState: DetailSiswa = uiStateSiswa.detailSiswa): Boolean {
+        return with(uiState) {
+            nama.isNotBlank() && alamat.isNotBlank() && telpon.isNotBlank()
+        }
+    }
