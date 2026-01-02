fb7d8c9329d17e6f6b1a124ed9ef412fa7677bd9
fahreziahmad <alfahreziahmadsyahyan@gmail.com>
2026-01-02 18:23:41 +0700

    Menambahkan rute navigasi Home

---
+++ app/src/main/java/com/example/myfirabase/view/route/DestinasiHome.kt
@@ -0,0 +1,9 @@
+package com.example.myfirabase.view.route
+
+import com.example.contohfirebase.R
+
+
+object DestinasiHome : DestinasiNavigasi {
+    override val route = "home"
+    override val titleRes = R.string.app_name
+}
